002c6f89b7fb664b0213ce3f48ffc54783164823
Othon Oliveira <othonoliveira08@gmail.com>
2022-09-21 16:03:03 -0300

    Alterações no SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+import Title from './Title';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <div className="planets">
+          {Planets.map((planet, index) => (
+            <PlanetCard
+              key={ index }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
